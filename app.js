934d61cfdafafc5764078141651b904b3c9b9924
dani980boo <daniel980boo@gmail.com>
2024-03-07 16:47:21 -0300

    version-danidanihahaha

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var fornecedoresRouter = require('./routes/fornecedores');
 
 var app = express();
 
@@ -20,7 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/fornecedores', fornecedoresRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
