bc0faf9071315e63af53d44faf82100d34ce86ad
Redes IFC <redes@lab32-pc01.sombrio.ifc.edu.br>
2024-03-08 16:44:20 -0300

    yj

--- app.js
+++ app.js
@@ -6,13 +6,14 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var esportesRouter = require('./routes/esportes');
 var estudantesRouter = require('./routes/estudantes');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -23,6 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/estudantes', estudantesRouter);
+app.use('/esportes', esportesRouter);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/esportes.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('Raiz');
+});
+
+router.get('/futebol', function(req, res, next) {
+    res.send('/futebol');
+  });
+
+  router.get('/surfe', function(req, res, next) {
+    res.send('/surfe');
+  });
+
+  router.get('/volei', function(req, res, next) {
+    res.send('/volei');
+  });
+
+module.exports = router;
