2e055b2b422922ffe04bd2e6b9a709a8aedce91b
Redes IFC <redes@lab32-pc01.sombrio.ifc.edu.br>
2024-03-15 15:10:07 -0300

    Mongoose

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const mongoose = require("mongoose");
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var fornecedoresRouter = require('./routes/fornecedores');
-
 
 var app = express();
 
@@ -20,6 +21,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Set `strictQuery: false` to globally opt into filtering by properties that aren't in the schema
+// Included because it removes preparatory warnings for Mongoose 7.
+// See: https://mongoosejs.com/docs/migrating_to_6.html#strictquery-is-removed-and-replaced-by-strict
+mongoose.set("strictQuery", false);
+
+// Define the database URL to connect to.
+const mongoDB = "mongodb://127.0.0.1/my_database";
+
+// Wait for database to connect, logging an error if there is a problem
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
