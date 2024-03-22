8f8444d2bcb8bce8fdd0c2b18aaed3266233e67c
DiogoBauer <ddavibauer@gmail.com>
2024-03-22 16:46:37 -0300

    VersãoComErro

---
+++ models/genre.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const GenreSchema = new Schema({
+  name: { type: String, required: true, minLenght: 3, maxLength: 100 },
+});
+
+// Virtual for author's full name
+GenreSchema.virtual("url").get(function () {
+  // We don´t use an arrow function as we´ll need the this object
+  return `/catalog/genre/${this._name}`;
+});
+
+// Export model
+module.exports = mongoose.model("Genre", GenreSchema);
