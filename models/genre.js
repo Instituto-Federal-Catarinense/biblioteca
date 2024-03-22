1c6fb41f9b0f6cd5bc8abf848ca53aac982795b4
Redes IFC <redes@lab32-pc01.sombrio.ifc.edu.br>
2024-03-22 12:00:56 -0300

    Versão_7

--- models/genre.js
+++ models/genre.js
@@ -3,26 +3,13 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const Genre = new Schema({
-    SchemaType: { type: String, required: true, maxLength: 100 },
+    name: { type: String, required: true,mininumLeght: 3, maxLength: 100 },
 });
 
-// Virtual for author's full name
 Genre.virtual("name").get(function () {
-  // To avoid errors in cases where an author does not have either a family name or first name
-  // We want to make sure we handle the exception by returning an empty string for that case
-  let fullname = "";
-  if (this. SchemaType) {
-    fullname = `${this.SchemaType}`;
-  }
-
-  return fullname;
-});
-
-// Virtual for author's URL
-Genre.virtual("url").get(function () {
-  // We don't use an arrow function as we'll need the this object
-  return `/catalog/Genre/${this._id}`;
-});
+    // We don't use an arrow function as we'll need the this object
+    return `/catalog/genre/${this._id}`;
+  });
 
 // Export model
 module.exports = mongoose.model("Genre", Genre);
