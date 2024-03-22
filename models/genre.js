ee87d8b6c0e164913e57f86053491a5946f378d3
Redes IFC <redes@lab32-pc01.sombrio.ifc.edu.br>
2024-03-22 15:14:29 -0300

    Versão_8

--- models/genre.js
+++ models/genre.js
@@ -2,14 +2,14 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const Genre = new Schema({
-    name: { type: String, required: true,mininumLeght: 3, maxLength: 100 },
+const GenreSchema = new Schema({
+    name: { type: String, required: true, minLength: 100 , maxLength: 100 }
 });
 
-Genre.virtual("name").get(function () {
-    // We don't use an arrow function as we'll need the this object
-    return `/catalog/genre/${this._id}`;
-  });
 
+GenreSchema.virtual("url").get(function () {
+  // We don't use an arrow function as we'll need the this object
+  return `/catalog/genre/${this._name}`;
+});
 // Export model
-module.exports = mongoose.model("Genre", Genre);
+module.exports = mongoose.model("GenreSchema", GenreSchema);
